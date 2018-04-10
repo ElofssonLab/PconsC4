37d261ebc873476f66c6f508361bc01dc5bf9746
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-04-10 16:48:02 +0200

    Move ranking contacts to utils

--- pconsc4/__init__.py
+++ pconsc4/__init__.py
@@ -1,2 +1,3 @@
 from .create_model import get_pconsc4
-from .run import predict, compute_ranking
+from .run import predict
+from . import utils
--- pconsc4/run.py
+++ pconsc4/run.py
@@ -75,15 +75,6 @@ def _predict(model, feat_dict, L):
     return dict(cmap=cmap, s_score=sscoremap)
 
 
-def compute_ranking(scores, min_separation=0):
-    N = scores.shape[0]
-    score_lst = []
-    for i in range(N - min_separation):
-        for j in range(i + min_separation, N):
-            score_lst.append((i + 1, j + 1, scores[i, j]))
-    score_lst.sort(key=lambda x: x[2], reverse=True)
-    return score_lst
-
 
 def predict(model, alignment):
     feat_dict, L = _generate_features(alignment)
