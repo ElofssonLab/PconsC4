fb331a6ff5775159e8d0bcfac6b8cca4ef5bf06f
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2018-05-17 16:30:58 +0200

    Allow for arbitrary info to be passed in

--- pconsc4/utils.py
+++ pconsc4/utils.py
@@ -20,6 +20,7 @@ def format_contacts_casp(scores, sequence, properties=None, min_sep=0, full_prec
                               group='PconsC4',
                               developer='Mirco Michel, David Men\\\'endez Hurtado and Arne Elofsson',
                               method='PconsC4')
+    known_keys = list(default_properties.keys())
 
     default_properties.update(properties)
     # Header:
@@ -28,10 +29,16 @@ def format_contacts_casp(scores, sequence, properties=None, min_sep=0, full_prec
                         "AUTHOR {author}",
                         "METHOD {method}",
                         "REMARK GROUP {group}",
-                        "REMARK DEVELOPER {developer}",
-                        "MODEL 1\n")).format(**default_properties)
+                        "REMARK DEVELOPER {developer}\n")).format(**default_properties)
+
+    # Any other information passed in properties will be added as a remark
+    for k in known_keys:
+        default_properties.pop(k)
 
     content = [header]
+    for key, value in default_properties:
+        content.append('REMARK {} {}\n'.format(key, value))
+    content.append("MODEL 1\n")
 
     # Save the sequence, wrapped at 50 chars for some reason.
     n = 50
@@ -45,7 +52,7 @@ def format_contacts_casp(scores, sequence, properties=None, min_sep=0, full_prec
     data = []
     for i in range(len(sequence) - 1):
         for j in range(i + 1, len(sequence)):
-            if abs(i -j) > min_sep:
+            if abs(i - j) > min_sep:
                 data.append((i, j, scores[i, j]))
 
     data.sort(key=lambda x: x[2], reverse=True)
