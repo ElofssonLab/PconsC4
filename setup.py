de20fede8f1c9f3ddc0a7f5067cd1a03b794e3ed
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2019-03-25 17:00:54 +0100

    bump version

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ else:
 
 setup(
     name='pconsc4',
-    version='0.2.1',
+    version='0.3',
     description='',
     url='https://github.com/ElofssonLab/PconsC4',
     author='Mirco Michel and David Menéndez Hurtado',
