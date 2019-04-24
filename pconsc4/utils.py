46931755485aeef1cd7180b092357428b063d52a
Arne Elofsson (ae18c) <arne@bioinfo.se>
2019-04-24 17:12:25 +0200

    Added Joergens Patch to utils.py

--- pconsc4/utils.py
+++ pconsc4/utils.py
@@ -8,74 +8,26 @@ def compute_ranking(scores, min_separation=0):
     return score_lst
 
 
-def format_contacts_casp(scores, sequence, properties=None, min_sep=0, full_precision=True):
-    scores = scores.squeeze()
-
-    # Set default values:
-    if properties is None:
-        properties = dict()
-
-    default_properties = dict(target_name='TARGET',
-                              author='',
-                              group='PconsC4',
-                              developer='Mirco Michel, David Men\\\'endez Hurtado and Arne Elofsson',
-                              method='PconsC4')
-    known_keys = list(default_properties.keys())
-
-    default_properties.update(properties)
-    # Header:
-    header = '\n'.join(("PFRMAT RR",
-                        "TARGET {target_name}",
-                        "AUTHOR {author}",
-                        "METHOD {method}",
-                        "REMARK GROUP {group}",
-                        "REMARK DEVELOPER {developer}\n")).format(**default_properties)
-
-    # Any other information passed in properties will be added as a remark
-    for k in known_keys:
-        default_properties.pop(k)
-
-    content = [header]
-    for key, value in default_properties.items():
-        content.append('REMARK {} {}\n'.format(key, value))
-    content.append("MODEL 1\n")
-
-    # Save the sequence, wrapped at 50 chars for some reason.
-    n = 50
-    content.extend(sequence[i:i + n] + '\n' for i in range(0, len(sequence), n))
-
-    if full_precision:
-        line = '{i} {j} 0 8 {score}\n'
-    else:
-        line = '{i} {j} 0 8 {score:.8f}\n'
-
-    data = []
-    for i in range(len(sequence) - 1):
-        for j in range(i + 1, len(sequence)):
-            if abs(i - j) > min_sep:
-                data.append((i, j, scores[i, j]))
-
-    data.sort(key=lambda x: x[2], reverse=True)
-    for i, j, sc in data:
-        content.append(line.format(i=i + 1, j=j + 1, score=sc))
-
-    content.append('END\n')
-    return ''.join(content)
-
-
-def format_ss3(ss3_pred):
-    codes = {0: 'H', 1: 'E', 2: 'C'}
-    return ''.join(codes[x] for x in ss3_pred.squeeze().argmax(axis=1))
-
-
 def format_contacts_cameo(scores, sequence, seqidx, filename_out, min_sep=0):
     scores = scores.squeeze()
 
     # Read sequence
-    seq = ''.join(line.strip() for line in open(sequence) if not line.startswith('>'))
+    f = open(sequence)
+    txt = []
+    for line in f:
+        txt.append(line)
+    f.close()
+    seq = txt[1]
+    seq = seq.strip()
 
     # Convert sequence to list with numbered residues
-    residues = [char + str(i + 1) for i, char in enumerate(seq)]
+    i = 0
+    residues = list()
+
+    while i < len(seq):
+        elem = seq[i] + str(i + 1)
+        residues.append(elem)
+        i += 1
 
     data = []
     for i in range(len(seq)):
@@ -87,11 +39,20 @@ def format_contacts_cameo(scores, sequence, seqidx, filename_out, min_sep=0):
     data.sort(key=lambda x: x[2], reverse=True)
 
     # Write lines
-    with open(filename_out, 'w') as f_out:
-        for i in range(len(data)):
-            a = data[i][0]
-            b = data[i][1]
-            prob = data[i][2]
-            current_line = 's{}.{}\ts1.{}\t0\t8\t{:.4f}\n'.format(seqidx, residues[a], residues[b], prob)
-            f_out.write(current_line)
+    line_list = list()
+    for i in range(len(data)):
+        a = data[i][0]
+        b = data[i][1]
+        prob = data[i][2]
+        current_line = "s%s.%s\ts1.%s\t0\t8\t%.4f" % (seqidx, residues[a], residues[b], prob)
+        line_list.append(current_line)
 
+    # Write lines into text file
+    txt = open(filename_out, 'a')
+    txt.write('\n'.join(line_list))
+    txt.close()
+
+
+def format_ss3(ss3_pred):
+    codes = {0: 'H', 1: 'E', 2: 'C'}
+    return ''.join(codes[x] for x in ss3_pred.squeeze().argmax(axis=1))
